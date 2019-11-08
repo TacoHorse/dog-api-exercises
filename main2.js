46ea42e1dcf077274427e9edc29c74be99ae6d8a
Sean <sean.cowan1@gmail.com>
2019-11-08 16:53:07 -0500

    exercises completed

--- main2.js
+++ main2.js
@@ -9,9 +9,9 @@ function handleUserInput() {
         e.preventDefault();
         let userInput = $('[name=num-of-dogs]').val();
         if (userInput) {
-            if (userInput <=50  && userInput > 0) {
+            if (userInput <= 50 && userInput > 0) {
                 getRandomDogs(userInput);
-            }  else {
+            } else {
                 alert("Your input must be between 1 and 50");
             }
         } else {
@@ -22,8 +22,13 @@ function handleUserInput() {
 
 function getRandomDogs(numberOfDogs) {
     fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogs}`)
-    .then(Response => Response.json())
-    .then(ResponseJson => console.log(ResponseJson))
-    .catch(error => alert('Something went wrong try again later.'));
+        .then(Response => Response.json())
+        .then(ResponseJson => {
+            $('.dog-images').empty();
+            for (let i = 0; i < ResponseJson.message.length; i++) {
+                $('.dog-images').append(`<img class="dog-pic" src="${ResponseJson.message[i]}">`);
+            }
+        })
+        .catch(error => alert('Something went wrong try again later.'));
 }
 $(initSite);
---
+++ main3.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function initSite() {
+    handleUserInput();
+}
+
+function handleUserInput() {
+    $('.dog-form').submit(e => {
+        e.preventDefault();
+        let breed = $('[name=breed]').val();
+            if (breed) {
+                getRandomDog(breed.toLowerCase());
+            } else {
+                alert("Please enter a dog breed");
+            }
+    });
+}
+
+function getRandomDog(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(Response => Response.json())
+        .then(ResponseJson => {
+            $('.dog-images').empty();
+            $('.dog-images').append(`<img class="dog-pic" src="${ResponseJson.message}">`);
+        })
+        .catch(error => alert(`${ResponseJson.message}`));
+}
+$(initSite);
