66d01602de0090be2249333d5b6108dc9eef05b9
Sean <sean.cowan1@gmail.com>
2019-11-11 13:57:28 -0500

    ex 3 changes

--- main3.js
+++ main3.js
@@ -8,21 +8,28 @@ function handleUserInput() {
     $('.dog-form').submit(e => {
         e.preventDefault();
         let breed = $('[name=breed]').val();
-            if (breed) {
-                getRandomDog(breed.toLowerCase());
-            } else {
-                alert("Please enter a dog breed");
-            }
+        if (breed) {
+            getRandomDog(breed.toLowerCase());
+        } else {
+            alert("Please enter a dog breed");
+        }
     });
 }
 
 function getRandomDog(breed) {
     fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-        .then(Response => Response.json())
+        .then(Response => {
+            console.log(Response.body);
+            if (Response.status === 200) {
+                return Response.json();
+            } else {
+                throw new Error(`Something went wrong.  Error code ${Response.status}`);
+            }
+        })
         .then(ResponseJson => {
             $('.dog-images').empty();
             $('.dog-images').append(`<img class="dog-pic" src="${ResponseJson.message}">`);
         })
-        .catch(error => alert('Something went wrong please check the URL and try again'))
+        .catch(e => alert(e));
 }
 $(initSite);
